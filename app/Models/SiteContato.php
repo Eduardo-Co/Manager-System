eab8d4cd950abde39895687bb2e9bd8e53f4afa3
Eduardo-Co <121448598+Eduardo-Co@users.noreply.github.com>
2024-02-29 09:27:43 -0300

    Commit 1

---
+++ app/Models/SiteContato.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+//Site_Contato
+//site_contato
+//site_contatos
+
+class SiteContato extends Model
+{
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contatos_id', 'mensagem'];
+}
